d6c3e6b8dc1b6c0976c96cc9569232ee40a8f322
t-izaki <>
2018-12-01 21:27:09 +0900

    Add login feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  get 'top/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  devise_for :admins, controllers: { sessions: 'admins/sessions' }
   root to: 'top#index'
 end
