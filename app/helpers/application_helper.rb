be9f6ad451e1f76305d29f326e33300d509eb147
t-izaki <>
2018-12-24 15:16:06 +0900

    Update layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,18 @@ module ApplicationHelper
     arr = %w[日 月 火 水 木 金 土]
     arr[date.wday]
   end
+
+  def nav_item_class(controller_name = '')
+    ['nav-item', 'nav-link', active_or_blank(controller_name)].reject(&:blank?).join(' ')
+  end
+
+  private
+
+  def active_or_blank(controller_name)
+    if controller.controller_name == controller_name
+      'active'
+    else
+      ''
+    end
+  end
 end
